9de9fc5976ddc655faa0ebe7f35924b5855cb751
2021t01252 <chamodlakshitha666@gmail.com>
2024-06-17 20:21:41 +0530

    sorting

---
+++ javalearn01/src/day7/Sorting_string.java
@@ -0,0 +1,22 @@
+package day7;
+
+import java.util.Arrays;
+
+public class Sorting_string {
+
+	public static void main(String[] args) {
+		
+		
+		char s[] = {'D','C','B','A'};
+		
+		System.out.println("Before sorting......."+Arrays.toString(s));
+		
+		Arrays.sort(s);
+		
+		System.out.println("After sorting........"+Arrays.toString(s));
+		
+	
+
+	}
+
+}
