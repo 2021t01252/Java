c9fef65d3a568585f6d3ae59b80094f06a7bfd48
2021t01252 <chamodlakshitha666@gmail.com>
2024-09-05 23:28:41 +0530

    ss

--- javalearn01/src/day1/First_java_program.java
+++ javalearn01/src/day1/First_java_program.java
@@ -3,7 +3,7 @@ package day1;
 public class First_java_program {
 	
 	public static void main(String args[]) {
-		System.out.println("Hello World");
+		System.out.println("Hello World_Hi");
 		System.out.println(10+20);
 	}
 
