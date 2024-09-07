aa39b97ccdb4cba6debb6ef7c86a922a5ea73ea9
2021t01252 <chamodlakshitha666@gmail.com>
2024-09-07 18:11:45 +0530

    String1

---
+++ javalearn01/src/day8/StringMethods.java
@@ -0,0 +1,27 @@
+package day8;
+
+public class StringMethods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s = "Welcome Chamod";
+		
+		// String s = new String("Welcome");
+		
+		//length() - return length of the string
+		
+		System.out.println(s.length());
+		
+		// concat() - join string
+		
+		String s1 = "Welcome ";
+		String s2 = "to Java";
+		
+		System.out.println(s1+s2);
+		
+		System.out.println(s1.concat(s2));
+		
+	}
+
+}
